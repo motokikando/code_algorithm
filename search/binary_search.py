dd74d4daa452da77a6bce4ffc6ccbaa23a9c7e08
motokikando <kando813motoki@gmail.com>
2021-10-15 15:32:22 +0900

    binary_search

---
+++ search/binary_search.py
@@ -0,0 +1,24 @@
+from typing import List
+from typing import NewType
+
+
+#型定義
+# IndexNum = NewType('IndexNum', int)
+
+def binary_search(numbers:List[int], value:int) -> int:
+    left, right = 0, len(numbers)-1
+    while left <= right:
+        mid = (left + right) // 2
+        if numbers[mid] == value:
+            return mid
+        elif numbers[mid] < value:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+    
+
+
+if __name__=='__main__':
+    nums = [0,1,5,7,9,11,15,20,24]
+    print(binary_search(nums, 24))
---
+++ search/linear_search.py
@@ -0,0 +1,19 @@
+from typing import List
+from typing import NewType
+
+#型定義
+IndexNum = NewType('IndexNum', int)
+
+def linear_search(numbers: List[int], value: int) -> IndexNum:
+    for i in range(0, len(numbers)):
+        if numbers[i] == value:
+            return i
+    return -1
+
+
+if __name__ == '__main__':
+     nums = [0, 1, 5, 7, 9, 11, 15, 20, 24]
+     print(linear_search(nums, 20))
+
+
+
