51cf26b9dd0474fd3631f855aa17dd23ef5f4285
motokikando <kando813motoki@gmail.com>
2021-10-16 17:39:29 +0900

    linked_list

---
+++ linked_list/simply_linked_list.py
@@ -0,0 +1,47 @@
+from typing import Any
+
+class Node(object):
+    def __init__(self, data:Any, next_node = None):
+        self.data = data
+        self.next = next_node
+
+class LinkedList(object):
+    def __init__(self, head=None) -> None:
+        self.head = head
+
+    def append(self, data:Any) -> None:
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def insert(self, data:Any) -> None:
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def print(self) -> None:
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+
+if __name__ == '__main__':
+    l = LinkedList()
+    l.append(1)
+    l.append(2)
+    l.append(3)
+    l.insert(0)
+    # print(l.head.data)
+    # print(l.head.next.data)
+    # print(l.head.next.next.data)
+    # print(l.head.next.next.next.data)
+    l.print()
+
+
+
+
