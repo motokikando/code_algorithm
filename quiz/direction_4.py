2728d03e51394fbfe826da10a7bb5722bb225490
motokikando <kando813motoki@gmail.com>
2021-11-20 16:07:28 +0900

    direction_4

---
+++ quiz/direction_4.py
@@ -0,0 +1,43 @@
+h, w, y, x, n = map(int, input().split())
+s = [list(input()) for i in range(h)]
+directions = ['N', 'E', 'S', 'W']
+now_direction = 0
+
+for i in range(n):
+  lr, k = input().split()
+  k = int(k)
+  if lr == 'L':
+    now_direction = (3+ now_direction)%4
+  else:
+    now_direction = (1+now_direction)%4
+  flag = False
+
+  for i in range(k):
+    if directions[now_direction] == 'N':
+      y -= 1
+    elif directions[now_direction] == 'S':
+      y += 1
+    elif directions[now_direction] == 'E':
+      x += 1
+    elif directions[now_direction] == 'W':
+      x -= 1
+
+    if x < 0 or x >= int(w) or y < 0 or y >= int(h) or s[y][x] == '#':
+      flag = True
+      if directions[now_direction] == 'N':
+        y += 1
+      elif directions[now_direction] == 'S':
+        y -= 1
+      elif directions[now_direction] == 'E':
+        x -= 1
+      elif directions[now_direction] == 'W':
+        x += 1
+      break
+  print(y,x)
+  if flag:
+    print('Stop')
+    break
+
+
+
+
