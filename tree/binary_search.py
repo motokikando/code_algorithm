79c8b1dd272e89afa12376c85f58d28ab9be8c49
motokikando <kando813motoki@gmail.com>
2021-11-25 09:49:27 +0900

    binary_search_class

--- tree/binary_search.py
+++ tree/binary_search.py
@@ -6,85 +6,104 @@ class Node(object):
         self.left = None
         self.right = None
 
-def insert(node, value):
 
-    if node is None:
-        return Node(value)
+class BinarySearchTree(object):
 
-    if value < node.value:
-        node.left = insert(node.left, value)
-    else:
-        node.right = insert(node.right, value)
+    def __init__(self) -> None:
+        self.root = None
 
-    return node
+    def insert(self, value: int) -> None:
 
-def inorder(node) -> None:
-    # Inorder Left, Root, Right
-    # Preorder Root, Left, Right
-    # Postorder Left, Right, Root
-    if node is not None:
-        inorder(node.left)
-        print(node.value)
-        inorder(node.right)
+        if self.root is None:
+            self.root = Node(value)
+            return
 
+        def _insert(node, value):
+            if node is None:
+                return Node(value)
 
-def search(node, value: int):
-    if node is None:
-        return False
+            if value < node.value:
+                node.left = _insert(node.left, value)
+            else:
+                node.right = _insert(node.right, value)
 
-    if node.value == value:
-        return True
-    elif node.value > value:
-        return search(node.left, value)
-    elif node.value < value:
-        return search(node.right, value)
+            return node
 
-#どれが最も小さいNodeかをみていく作業
-def min_value(node):
-    current = node
-    while current.left is not None:
-        current = current.left
-    return current
+        _insert(self.root, value)
 
+    def inorder(self) -> None:
+        def _inorder(node) -> None:
+            # Inorder Left, Root, Right
+            # Preorder Root, Left, Right
+            # Postorder Left, Right, Root
+            if node is not None:
+                _inorder(node.left)
+                print(node.value)
+                _inorder(node.right)
+        _inorder(self.root)
 
-def remove(node, value):
-    if node is None:
-        return node
+    def search(self, value:int) -> bool:
+        def _search(node, value: int):
+            if node is None:
+                return False
 
-    if value < node.value:
-        node.left = remove(node.left, value)
-    elif value > node.value:
-        node.right = remove(node.right,value)
+            if node.value == value:
+                return True
+            elif node.value > value:
+                return _search(node.left, value)
+            elif node.value < value:
+                return _search(node.right, value)
+        return _search(self.root, value)
 
-    else:
-        if node.left is None:
-            return node.right
-        elif node.right is None:
-            return node.left
+    #どれが最も小さいNodeかをみていく作業
+    def min_value(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
 
-        temp = min_value(node.right)
-        node.value = temp.value
-        node.right = remove(node.right, temp.value)
-    return node
+    def remove(self, value: int) -> None:
+        def _remove(node, value):
+            if node is None:
+                return node
+
+            if value < node.value:
+                node.left = _remove(node.left, value)
+            elif value > node.value:
+                node.right = _remove(node.right,value)
+
+            else:
+                if node.left is None:
+                    return node.right
+                elif node.right is None:
+                    return node.left
+
+                temp = self.min_value(node.right)
+                node.value = temp.value
+                node.right = _remove(node.right, temp.value)
+            return node
+        return _remove(self.root, value)
 
 
 
 
 if __name__ == '__main__':
+    binary_tree = BinarySearchTree()
+    binary_tree.insert(3)
+    binary_tree.insert(6)
+    binary_tree.insert(5)
+    binary_tree.insert(7)
+    binary_tree.insert(10)
+    binary_tree.insert(2)
+
+    binary_tree.inorder()
     root = None
-    root = insert(root, 3)
-    root = insert(root, 6)
-    root = insert(root, 5)
-    root = insert(root, 7)
-    root = insert(root, 1)
-    root = insert(root, 10)
-    root = insert(root, 2)
     # inorder(root)
     # print(root.value)
     # print(root.right.value)
     # print(root.right.left.value)
     # print(root.right.right.value)
-    inorder(root)
-    print('############# Remove')
-    root = remove(root, 6)
-    inorder(root)
+    # inorder(root)
+    # print('############# Remove')
+    # root = remove(root, 6)
+    # inorder(root)
