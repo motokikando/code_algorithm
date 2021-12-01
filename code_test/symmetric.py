51297a6f32c5bf9cb7da9f6b520d84c35b55280f
motokikando <kando813motoki@gmail.com>
2021-12-01 13:08:01 +0900

    修正

--- code_test/symmetric.py
+++ code_test/symmetric.py
@@ -19,3 +19,21 @@ if __name__ == '__main__':
     l =  [(1,2),(3,5),(4,7),(5,3),(7,4)]
     for r in find_pair(l):
         print(r)
+
+
+
+
+# from typing import List, Iterator, Tuple
+
+# def find_pair(pairs: List[Tuple[int, int]]) -> Iterator[Tuple[int,int]]:
+#     cache = {}
+#     for pair in pairs:
+#         first, second = pair[0], pair[1]
+#         value = cache.get(second)
+#         if not value:
+#             #辞書型として定義
+#             cache[first] = second
+#         elif value == first:
+#             yield pair
+
+
