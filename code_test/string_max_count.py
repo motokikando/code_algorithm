f099a0bc87f8918beea9aeb4fc059ac9dd94454d
motokikando <kando813motoki@gmail.com>
2021-12-01 14:27:51 +0900

    修正

--- code_test/string_max_count.py
+++ code_test/string_max_count.py
@@ -1,9 +1,10 @@
 from collections import Counter
 from typing import Tuple
+import operator
 
 def string_max_count(s: str) -> None:
 
-    count = {}
+    count = {} #count = dict()でもOK
     for i in s:
         if i in count:
             #count[]で値を取得し，lower()で小文字に変換する
@@ -18,11 +19,16 @@ def string_max_count(s: str) -> None:
     print(count2[0])
 
 
+def string_max_count2(strings: str) -> Tuple[str, int]:
+    strings = strings.lower()
+    l = [(c, strings.count(c)) for c in strings if not c.isspace()]
+
+    # 最大値を取得
+    return max(l, key=operator.itemgetter(1))
+
+
 
 if __name__ == '__main__':
     s = 'This is a pen. This is an apple. Applepen'
-    string_max_count(s)
-
-
-
-
+    # string_max_count(s)
+    print(string_max_count2(s))
