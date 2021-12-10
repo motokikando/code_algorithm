7e090687c92a1ea97bad827e7d78692c220d67d8
motokikando <kando813motoki@gmail.com>
2021-12-11 00:07:18 +0900

    .

--- code_test/tempCodeRunnerFile.py
+++ code_test/tempCodeRunnerFile.py
@@ -1,8 +1,2 @@
-name__ == '__main__':
-#     for i in range(10):
-#         print(long_func(i))
 
-#     start = time.time()
-#     for i in range(10):
-#         print(long_func(i))
-#     print(time
+list_append(10)
