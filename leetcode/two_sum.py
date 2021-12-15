5595e5b5fc2a483cc2d7a55c177f25279d585ce9
motokikando <kando813motoki@gmail.com>
2021-12-15 14:21:56 +0900

    two_sum

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -1,21 +1,36 @@
 from typing import List
 class Solution:
-    def two_Sum(self, nums: List[int], target:int) -> List[int]:
+    def twoSum(self, nums: List[int], target:int) -> List[int]:
         output = []
-        j = 0
+        pivot = 0
+        # targetより大きい値を除外
         for i in reversed(range(len(nums))):
             if nums[i] > target:
                nums.pop(i)
+
+        flag = False
         for i in range(len(nums)):
-            print(nums[j])
+            for j in range(pivot+1, len(nums)):
+                if nums[pivot]+nums[j] == target:
+                    output.extend([pivot,j])
+                    flag = True
+                    break
+            if flag:
+                break
+            pivot += 1
+        return output
+class Solution:
+    def twoSum(self, nums: List[int], target: int):
+        output = {}
+        for idx, val in enumerate(nums):
+            if target - val in output:
+                return (output[target-val], idx)
+            output[val] = idx
 
-
-
-
-        return nums
 
 if __name__== '__main__':
     s = Solution()
-    a = [2,7,11,15]
+    a = [2, 7, 11, 15]
     b = 9
-    print(s.two_Sum(a, b))
+    print(s.twoSum(a, b))
+
