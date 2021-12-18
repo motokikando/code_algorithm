7b08335bd1da7b15edfa1cabff5d8ce652f74ba6
motokikando <kando813motoki@gmail.com>
2021-12-18 16:03:48 +0900

    add

---
+++ leetcode/longest_palindromic_substring.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s:str) -> str:
+        if len(s) == 1 or s == s[::-1]:
+            return s
+        count = 0
+        ans = ''
+        for i in range(len(s)):
+            for j in range(i, len(s)+1):
+                s_part = s[i:j]
+                if s_part == s_part[::-1] and len(s_part) > count:
+                    ans = s_part
+                    count = len(s_part)
+        return ans
+
+
+
+
+
+
+
+
+if __name__== '__main__':
+    a = Solution()
+    string = 'bgjgikuoukskjgsk'
+    print(a.longestPalindrome(string))
+
---
+++ leetcode/search_insert_position.py
@@ -0,0 +1,25 @@
+from typing import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        ans = 0
+        for i in range(len(nums)):
+            if nums[i] == target:
+                ans = i
+                break
+            elif target < nums[i]:
+                nums.insert(i, target)
+                ans = i
+                break
+            elif target > nums[-1]:
+                ans = len(nums)
+        return ans
+
+
+
+
+if __name__ == '__main__':
+    a = [1, 3, 5, 6 , 8, 10 ,12 ]
+    t = 14
+    s = Solution()
+    print(s.searchInsert(a, t))
+
