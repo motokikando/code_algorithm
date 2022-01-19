d498312f2bca2d6f7e6f17b2c7a7861c3db97351
motokikando <kando813motoki@gmail.com>
2022-01-19 19:36:52 +0900

    add

--- atcoder/4.eratosthenes/eratosthenes_2.py
+++ atcoder/4.eratosthenes/eratosthenes_2.py
@@ -20,7 +20,7 @@ def is_prime3(num:int) -> bool:
     36 = 12 * 3
     36 = 18 * 2
     36 = 36 * 1
-
+    
     """
     if num <= 1:
         return False
@@ -45,3 +45,19 @@ def is_prime4(num:int) -> bool:
             return False
     return False
 print(is_prime4(2))
+
+def is_prime5(num:int) -> bool:
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0 and num % 3 == 0:
+        return False
+
+    for i in range(5, math.floor(num**0.5)+1, 6):
+        if num % i == 0 or num % (i+2) == 0:
+            return False
+
+    return True
+
+print(is_prime5(37))
