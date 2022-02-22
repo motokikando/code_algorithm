3732c812d71160d905e5fed662dac51c6e3d2b07
motokikando <kando813motoki@gmail.com>
2022-02-22 16:54:34 +0900

    add

--- ABC/Easy/rally.py
+++ ABC/Easy/rally.py
@@ -3,4 +3,14 @@ n = int(input())
 x = list(map(int, input().split()))
 
 def get_power(n:int, x:List[int]) -> int:
-    
+    x = sorted(x)
+    ans = 0
+    avg = round(sum(x)/n)
+    for i in range(n):
+        ans += (avg-x[i])**2
+    return ans
+
+
+print(get_power(n,x))
+
+
