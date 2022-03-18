c93ab78f14087d1121791bf9d674a891e7485c12
motokikando <kando813motoki@gmail.com>
2022-03-18 22:10:38 +0900

    update

--- ABC/Easy/b_collatz.py
+++ ABC/Easy/b_collatz.py
@@ -1,4 +1,15 @@
-s = int(input())
+S = int(input())
 
 def get_ans(s:int )-> int:
-    
+    i = 1
+    nums = set()
+    while s not in nums:
+        nums.add(s)
+        i += 1
+        if s % 2 == 0:
+            s //= 2
+        elif s % 2 == 1:
+            s = 3*s+1
+    return i
+print(get_ans(S))
+
