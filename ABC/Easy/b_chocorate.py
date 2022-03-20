1349427efb1a0abd39e09fdde8e0946ec2d7410a
motokikando <kando813motoki@gmail.com>
2022-03-20 18:39:36 +0900

    update

---
+++ ABC/Easy/b_chocorate.py
@@ -0,0 +1,28 @@
+from typing import List
+N = int(input())
+D, X = map(int, input().split())
+A = [int(input()) for i in range(N)]
+
+
+def sum_chocorate_nums(d:int, x:int, a:List[int]) -> int:
+    nums = 0
+    for i in range(len(a)):
+        nums += get_chocorate_num(a[i], d)
+    return x + nums
+
+def get_chocorate_num(a:int, d:int) -> int:
+    num = 0
+    i = 0
+    while i*a+1 <= d:
+        num += 1
+        i += 1
+    return num
+
+print(sum_chocorate_nums(D, X, A))
+
+
+n,d,x,*a=map(int,open(0).read().split())
+print(a)
+s=[print(range(1,d+1,c)) for c in a]
+print(s)
+# print(sum(s)+x)
