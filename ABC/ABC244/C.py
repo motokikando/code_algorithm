edbb79105542c271fb514dd1ce1e6b66e1b92e82
motokikando <kando813motoki@gmail.com>
2022-03-25 21:21:55 +0900

    add

--- ABC/ABC244/C.py
+++ ABC/ABC244/C.py
@@ -1,32 +1,12 @@
-import sys
-import random
-from typing import List
-n = int(input())
-def gen_f():
-    flag = True
-    def f():
-        nonlocal flag
-        if flag:
-            flag = False
-            return True
-        else:
-            return False
-    return f
-f = gen_f()
-if f:
-    l = [i+1 for i in range(2*n+1)]
-    ans = random.choice(l)
-    l.pop(ans-1)
-    print(ans, flush=True)
 
+def main():
+    n = int(input())
+    rem = set(range(1, 2*n+2))
 
-def takahashi(n, l:List[int]):
-    if len(l)==1:
-        sys.exit()
-    else:
-        l.pop(n-1)
-        ans = random.choice(l)
-        l.pop(ans-1)
-        return ans
-
-print(takahashi(n, l),flush=True)
+    while True:
+        print(rem.pop(), flush=True)
+        a = int(input())
+        if a == 0:
+            break
+        rem.discard(a)
+main()
---
+++ ABC/Easy/b_niceshopping.py
@@ -0,0 +1,29 @@
+# from typing import List
+# A, B, M = list(map(int, input().split()))
+# a = list(map(int, input().split()))
+# b = list(map(int, input().split()))
+# X = [list(map(int, input().split()) for i in range(M))]
+
+# def get_resonable(a:List[int], b:List[int], X) -> int:
+#     #最小値を追加する
+#     ans = [min(a)+ min(b)]
+
+#     #二次元配列のループをする
+#     for i, x in enumerate(X):
+#         a_idx, b_idx, c = x
+#         ans.append(a[a_idx-1]+b[b_idx-1]-c)
+#     return min(ans)
+
+
+# print(get_resonable(a, b, X))
+A, B, M = list(map(int, input().split()))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+X = [list(map(int, input().split())) for i in range(M)]
+
+ans = [min(a) + min(b)]
+for x in X:
+    a_idx, b_index, c = x
+    ans.append(a[a_idx - 1] + b[b_index - 1] - c)
+
+print(min(ans))
