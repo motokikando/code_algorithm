9026ba67332cd553f5f0fb424825e744272abedb
motokikando <kando813motoki@gmail.com>
2022-04-07 16:01:39 +0900

    update

---
+++ ABC/Easy/b_foods.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+K = [list(map(int, input().split())) for i in range(N)]
+[k.pop(0) for k in K]
+
+
+def count_loved_foods(L, M) ->int:
+    ans = [i+1 for i in range(M)]
+    for l in L:
+        ans = set(ans) & set(l)
+    return len(ans)
+
+print(count_loved_foods(K, M))
+
+
