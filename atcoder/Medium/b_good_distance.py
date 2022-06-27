249521826de413b3ac4719b3b0fda4c737614444
motokikando <kando813motoki@gmail.com>
2022-06-27 16:35:43 +0900

    update

--- atcoder/Medium/b_good_distance.py
+++ atcoder/Medium/b_good_distance.py
@@ -2,21 +2,18 @@
 #nC2回取得を行う
 #それぞれの点の距離を求める
 import math
-# from math import dist
 import itertools
 N, D = map(int, input().split())
 A = list(list(map(int, input().split())) for i in range(N))
 
-
 def get_int_distance(a, d):
     cnt = 0
     for v in itertools.combinations(a, 2):
-        # print(dist(v[0], v[1]))
         sum = 0
         for j in range(d):
             sum += (v[0][j]- v[1][j])**2
         if math.sqrt(sum) % 1 == 0:
             cnt += 1
-
     return cnt
+
 print(get_int_distance(A, D))
